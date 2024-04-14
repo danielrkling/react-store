47dce898e7785a2c3141062627c05fd139f755d2
Daniel Kling <danielrkling@gmail.com>
2024-04-14 13:19:25 -0400

    refactored to class for easier documentation

--- index.ts
+++ index.ts
@@ -1,170 +1,109 @@
-/**
- * This module contains the create store function to initialize a global store.
- * @module
- */
-
 import { useSyncExternalStore, useRef } from "react";
 
-export type GetState<T> = {
-  /**
-   * Get current state of store.
-   * @example
-   * const firstName = personStore.get().firstName
-   */
-  (): T;
-  /**
-   * Get current state of store via selector.
-   * @example
-   * const lastName = personStore.get(p=>p.lastName)
-   */
-  <R>(selector: (state: T) => R): R;
-};
-
-export type SetState<T> = {
-  /**
-   * Set new state based on previous state.
-   * @example
-   * personStore.set(prev=>({...prev, firstName: "Bob"}))
-   */
-  (setter: (prev: T) => T): T;
-  /**
-   * Set entirely new state.
-   * @example
-   * personStore.set({firstName: "Bob", lastName: "Johnson"}))
-   */
-  (newState: T): T;
-};
-
-export type UseState<T> = {
-  /**
-   * Rerenders component on all changes
-   * @example
-   * const person = personStore.use()
-   */
-  (): T;
-  /**
-   * Rerenders component on changes to result of selector
-   * @example
-   * const firstName = personStore.use(p=>p.firstName)
-   */
-  <R>(selector: (state: T) => R): R;
-  /**
-   * Rerenders component on changes to result of selector based on provided isEqual function
-   * @example
-   * const person = personStore.use(p=>p.id, (a,b)=>a==b)
-   */
-  <R>(selector: (state: T) => R, isEqual: (a: R, b: R) => boolean): R;
-  /**
-   * Rerenders component based on provided isEqual function
-   * @example
-   * const person = personStore.use(null, (a,b)=>a.id==b.id)
-   */
-  (selector: null, isEqual: (a: T, b: T) => boolean): T;
-};
-
-export type Subscribe<T> = {
-  /**
-   * Adds listener function to store. Runs on all changes.
-   * Returns method to unsubscribe
-   * @example
-   * const removeListener = personStore.subscribe(person=>{
-   *    console.log(person)
-   * })
-   * removeListener()
-   */
-  (listener: (state: T) => void): () => void;
-};
-
-export type Store<T> = {
-  /**
-   * Get current state of store.
-   * @example
-   * const firstName = personStore.get().firstName
-   * const lastName = personStore.get(p=>p.lastName)
-   */
-  get: GetState<T>;
-  /**
-   * Set new state of store and trigger all listeners.
-   * @example
-   * personStore.set(prev=>({...prev, firstName: "Bob"})) //use current state
-   * personStore.set({firstName: "Bob", lastName: "Johnson"})) //set entirely new state
-   */
-  set: SetState<T>;
-  /**
-   * Hook to subscribe React component to store.
-   * Selector allows any derived value.
-   * Then compares with Object.is (React default) or custom isEqual function.
-   * @example
-   * //will rerender on any changes made to the store
-   * const {firstName} = personStore.use()
-   * //Will only rerender on changes in lastName
-   * const lastName = personStore.use(p=>p.lastName)
-   * //Will only rerender if whole year age is different
-   * const roundedAge = personStore.use(p=>p.age,(a,b)=>Math.floor(a)===Math.floor(b))
-   */
-  use: UseState<T>;
-  /**
-   * Adds listener function to store. Runs on all changes.
-   * Returns method to unsubscribe
-   * @example
-   * const removeListener = personStore.subscribe(person=>{
-   *    console.log(person)
-   * })
-   * removeListener()
-   */
-  subscribe: Subscribe<T>;
-};
-
 /**
- * Creates a store that manages state and provides methods for setting, getting, and merging state values.
- * @template T The type of the store state
- * @param [initialState] The initial state of the store
- * @returns The store object, which includes the set, get, use, and subscribe methods
- * @example
- * const personStore = createStore({
+ * Creates a store that manages state and provides methods for setting, getting, and using state values.
+ * ```ts
+ * const personStore = new Store({
  *      firstName: "Bob",
  *      lasteName: "Johnson"
  * })
+ * ```
  */
-export function createStore<T>(initialState?: T): Store<T> {
-  const subscriptions = new Set<(state?: T) => void>();
+export class Store<T> {
+  private state: T;
+  private subscriptions: Set<(state?: T) => void>;
 
-  let state = initialState;
+  constructor(initialState?: T) {
+    this.state = initialState;
+    this.subscriptions = new Set<(state?: T) => void>();
+  }
 
-  function get<R>(selector?: (state: T) => R) {
+  /**
+   * Get current state of store.
+   * ```ts
+   * const firstName = personStore.get().firstName
+   * ```
+   */
+  get(): T;
+  /**
+   * Get current state of store via selector.
+   * ```ts
+   * const lastName = personStore.get(p=>p.lastName)
+   * ```
+   */
+  get<R>(selector: (state: T) => R): R;
+  get<R>(selector?: (state: T) => R) {
     if (selector instanceof Function) {
-      return selector(state);
+      return selector(this.state);
     } else {
-      return state;
+      return this.state;
     }
   }
 
-  function set(setter: T | ((prev: T) => T)): T {
-    state = setter instanceof Function ? setter(state) : setter;
-    subscriptions.forEach((listener) => listener(state));
-    return state;
+  /**
+   * Set entirely new state.
+   * ```ts
+   * personStore.set({firstName: "Bob", lastName: "Johnson"}))
+   * ```
+   */
+  set(newState: T): T;
+  /**
+   * Set new state based on previous state.
+   * ```ts
+   * personStore.set(prev=>({...prev, firstName: "Bob"}))
+   * ```
+   */
+  set(setter: (prev: T) => T): T;
+  set(setter: T | ((prev: T) => T)): T {
+    this.state = setter instanceof Function ? setter(this.state) : setter;
+    this.subscriptions.forEach((listener) => listener(this.state));
+    return this.state;
   }
 
-  function use(
-    selector?: (state: T) => any,
-    equals?: (a: any, b: any) => boolean
-  ) {
-    const snapshot = equals
-      ? () => get(useEqual<T, T>(selector, equals))
-      : () => get(selector);
-    return useSyncExternalStore<T>(subscribe, snapshot);
+  /**
+   * Subscribe component to rerender on any state change
+   * ```ts
+   * const person = personStore.use()
+   * ```
+   */
+  use(): T;
+  /**
+   * Subscribe component to rerender on selected value change
+   * ```ts
+   * const firstName = personStore.use(p=>p.firstName)
+   * ```
+   */
+  use<R>(selector: (state: T) => any): R;
+  use(selector?: (state: T) => any) {
+    const snapshot = () => this.get(selector);
+    return useSyncExternalStore<T>(this.subscribe, snapshot);
   }
 
-  function subscribe(listener: (state: T) => void) {
-    subscriptions.add(listener);
+  /**
+   * Subscribe listener to get called on any state change
+   * Returns method to unsubscribe
+   * ```ts
+   * const removeListener = personStore.subscribe(person=>{
+   *    console.log(person)
+   * })
+   * removeListener()
+   * ```
+   */
+  subscribe(listener: (state: T) => void) {
+    this.subscriptions.add(listener);
     return () => {
-      subscriptions.delete(listener);
+      this.subscriptions.delete(listener);
     };
   }
-
-  return { get, set, use, subscribe };
 }
 
+
+/**
+ * Hook to only update state if different using custom equal function
+ * @param selector select dervied value
+ * @param equals function to compare derived value
+ * @returns updater function to pass to set method
+ */
 export function useEqual<S, U>(
   selector: (state: S) => U,
   equals: (a: any, b: any) => boolean
@@ -178,5 +117,3 @@ export function useEqual<S, U>(
       : (prev.current = next);
   };
 }
-
-export default createStore
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # React Store
 
-Global state for react 18 that allows components or any listener to individually subscribe to any part of the state.
+Global state for React 18 that allows components or any listener to individually subscribe to any part of the state.
 
 ## Description
 
-An in-depth paragraph about your project and overview of use.
+This library provides a lightweight state management solution for React applications. It offers a simple Store class for managing application state and a useEqual hook for optimizing updates based on custom comparison logic.
 
 ## Getting Started
 
@@ -18,49 +18,63 @@ An in-depth paragraph about your project and overview of use.
 npx jsr add @kling/react-store
 ```
 
-```ts
-import * as mod from "@kling/react-store";
-```
-
 ### Usage
 
 ```tsx
-import { createStore } from "@kling/react-store";
+import { Store } from "@kling/react-store";
 
-const personStore = createStore({
+const personStore = new Store({
   firstName: "",
   lastName: "",
 });
 
+//You can use the set function anywhere in your app
+function resetName() {
+  personStore.set({
+    firstName: "",
+    lastName: "",
+  });
+}
+
+//This component rerenders on all changes to personStore
 function App() {
   const person = personStore.use();
 
-  return 
-    <div>
-      <PersonName />
-      <input
-        value={person.firstName}
-        onChange={(e) => {
-          personStore.set((prev) => ({ ...prev, firstName: e.target.value }));
-        }}
-      />
-    </div>
-  
+  return;
+  <div>
+    {person.firstName}
+    <LastName />
+    <input
+      value={person.firstName}
+      onChange={(e) => {
+        personStore.set((prev) => ({ ...prev, firstName: e.target.value }));
+      }}
+    />
+    <SetNameButton />
+  </div>;
 }
 
-function PersonName() {
-  return (
-    <div>
-      {person.firstName}
-      {person.lastName}
-    </div>
-  );
+//Seperate Component. Only rerenders on changes to lastName
+function LastName() {
+  const lastName = personStore.use((p) => p.lastName);
+
+  return <div>{lastName}</div>;
 }
+
+//Does not subscribe, will not rerender on state changes
+function SetNameButton() {
+  return <button onClick={()=>resetName()}>Reset</button>
+}
+
+
+
+
 ```
 
 ### Notes
 
-- If you want to store a function, set state must use the function method.
+- If you want to store a function, set state must use the function method. Otherwise it will call your function and store its return value as the new state.
+- Works well with immer for lots of deeply nested state changes if desired
 
 ```ts
 import { createStore } from "@kling/react-store";
@@ -70,9 +84,8 @@ const functionStore = createStore(() => console.log("initial function"));
 functionStore.set(() => () => console.log("second function"));
 ```
 
-
 ## Acknowledgments
 
-I was inspired much by Zustand, but I wanted a different way to interact with the store.
+I was inspired much by Zustand, but I wanted a different way to interact with the store. Add keep set actions outside the rendering cycle.
 
 - [Zustand](https://github.com/pmndrs/zustand/tree/main)
